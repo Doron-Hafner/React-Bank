f996718017e08845c4c38d6e25bc212578548aaf
Doron Hafner <dh9707@gmail.com>
2020-10-27 02:53:42 +0200

    fix router name

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,24 @@
 const express = require('express')
-const transactionRouter = express.Router()
+const router = express.Router()
 const Transaction = require('../model/Transaction')
 
-transactionRouter.get('/transactions', async (req, res) => {
+router.get('/transactions', async (req, res) => {
     const transactions = await Transaction.find({})
     res.send(transactions)
 })
   
-transactionRouter.post('/transaction', async (req, res) => {
+router.post('/transaction', async (req, res) => {
     const newTransaction = new Transaction(req.body)
     const saveTransaction = await newTransaction.save()
     res.send(saveTransaction)
 })
 
-transactionRouter.delete('/transaction/:id', async (req , res) =>{
+router.delete('/transaction/:id', async (req , res) =>{
     const removeTransaction = await Transaction.findByIdAndDelete(req.params.id)
     res.send(removeTransaction)
 })
 
-transactionRouter.get('/catagories', async (req, res) => {
+router.get('/catagories', async (req, res) => {
     const catagories = await Transaction.aggregate([
         {$group: {_id: "$catagory", total: {$sum: "$amount"}} }
         ])
@@ -26,4 +26,4 @@ transactionRouter.get('/catagories', async (req, res) => {
 })
 
 
-module.exports = transactionRouter
+module.exports = router
