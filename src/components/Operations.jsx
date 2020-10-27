e46d8189ddb978e2adc3c656a1eb8581e9690095
Doron Hafner <dh9707@gmail.com>
2020-10-27 02:41:13 +0200

    minor change

--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import '../App.css'
 import 'fontsource-roboto';
-import { TextField, Box, Button, ButtonGroup} from '@material-ui/core/'
+import { TextField, Box, Button, ButtonGroup } from '@material-ui/core/'
 
 export default class Operations extends Component {
     constructor() {
@@ -13,42 +13,44 @@ export default class Operations extends Component {
         }
     }
     handleInput = e => {
-        const value = e.target.value,
+        let value = e.target.value,
             name = e.target.name
-        this.setState({ [name]: value })
+        if (name === 'amount') 
+            value = Number(value)
+        this.setState({ [name]: value }
+        )
     }
-    handleClick = (e) => {
+    handleClick = e => {
         if (this.state.amount && this.state.vendor && this.state.catagory) {
             let transaction = { ...this.state }
-            if (e.target.value === 'withdraw') {
-                transaction.amount = Number(transaction.amount * -1)
-                this.setState({ transaction: transaction }, () => this.props.addTransaction(this.state))
+            if (e.currentTarget.value === 'withdraw') {
+                transaction.amount = transaction.amount * -1
+                this.setState(transaction, () =>
+                    this.props.addTransaction(this.state)
+                )
             } else {
                 this.props.addTransaction(this.state);
             }
-        }
+        } else { alert('all fields required') }
     }
     componentWillUnmount() {
         this.props.redirect()
     }
-
     render() {
-        const vendor = this.state.vendor.replace(/[^a-zA-Z ]/, '')
-        const catagory = this.state.catagory.replace(/[^a-zA-Z ]/, '')
         return (
-            <Box marginTop={5}>
+            <Box marginTop={5} style={{ textAlign: 'center' }}>
                 <Box>
-                    <TextField type="number" name="amount" value={this.state.amount.replace(/-/g, '')} onChange={this.handleInput} placeholder='Amount'></TextField>
+                    <TextField type="number" name="amount" value={this.state.amount} onChange={this.handleInput} placeholder='Amount'></TextField>
                 </Box>
                 <Box>
-                    <TextField type="text" name="vendor" value={vendor} onChange={this.handleInput} placeholder='Vendor'></TextField>
+                    <TextField type="text" name="vendor" value={this.state.vendor} onChange={this.handleInput} placeholder='Vendor'></TextField>
                 </Box>
                 <Box marginBottom={5}>
-                    <TextField type="text" name="catagory" value={catagory} onChange={this.handleInput} placeholder='Catagory'></TextField>
+                    <TextField type="text" name="catagory" value={this.state.catagory} onChange={this.handleInput} placeholder='Catagory'></TextField>
                 </Box>
                 <ButtonGroup disableElevation variant="contained">
-                    <Button onClick={this.handleClick} value='deposit' style={{backgroundColor:'limegreen' }}> Deposit</Button>
-                    <Button onClick={this.handleClick} value='withdraw' style={{backgroundColor:'crimson' }}>Withdraw </Button>
+                    <Button onClick={this.handleClick} value='deposit' style={{ backgroundColor: 'limegreen' }}> Deposit</Button>
+                    <Button onClick={this.handleClick} value='withdraw' style={{ backgroundColor: 'crimson' }}>Withdraw </Button>
                 </ButtonGroup>
             </Box>
         )
